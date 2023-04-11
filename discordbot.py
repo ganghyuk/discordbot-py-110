d4420c5fd983894c02d614f2e5c76d650c8b29a8
ganghyuk <130384526+ganghyuk@users.noreply.github.com>
2023-04-11 19:45:25 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,8 +22,11 @@ async def on_message(message):
     if message.content == f'{PREFIX}call':
         await message.channel.send("callback!")
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith(f'{PREFIX}!출근'):
+        await message.channel.send(" {}, 님이 출근하셨습니다".format( message.author.mention))
+        
+    if message.content.startswith(f'{PREFIX}!퇴근'):
+        await message.channel.send(" {}, 님이 퇴근하셨습니다".format( message.author.mention))
 
 
 try:
@@ -32,20 +35,8 @@ except discord.errors.LoginFailure as e:
     print("Improper token has been passed.")
     import discord, asyncio
 
-client = discord.Client(intents = discord.Intents.all())
 
-@client.event
-async def on_ready(): 
-    print("이 문장은 Python의 내장 함수를 출력하는 터미널에서 실행됩니다\n지금 보이는 것 처럼 말이죠")
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("문의는 강혁#1647으로 디엠해주세요"))
 
-@client.event
-
-async def on_message(message):
-    if message.content == "!출근": 
-        await message.channel.send (" {}, 님이 출근하셨습니다".format( message.author.mention))
-    if message.content == "!퇴근": # 메세지 감지
-        await message.channel.send (" {}, 님이 퇴근하셨습니다".format( message.author.mention))
     
 
 
