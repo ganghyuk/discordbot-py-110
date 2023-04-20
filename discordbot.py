571b7400e32d9cf30e644d78b1b366b77ec149d4
ganghyuk <130384526+ganghyuk@users.noreply.github.com>
2023-04-20 23:28:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,7 +13,7 @@ client = discord.Client(intents = discord.Intents.all())
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}.')
-    await client.change_presence(status=discord.Status.online, activity=discord.Game(" 서포트서버 https://discord.gg/spongebob-squarepants "))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(" 강혁최강 "))
 
 @client.event
 
